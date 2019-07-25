c49674e5db02e761f99dd21833e8e3b7b92d6eba
Dan Fulcher <me@danfulcher.co.uk>
2019-07-25 19:00:01 +0100

    initial setup. Started state work

---
+++ store/questions.js
@@ -0,0 +1,10 @@
+export const state = () => ({
+    questions: [
+        {
+            sentence: 'This is a test sentence',
+            blanks: [
+                'test'
+            ]
+        },
+    ]
+})
